85d5b320daf5f8e1a89d609eaf9a6ee3e073b88e
Junior Moraes <juniorfvox@gmail.com>
2016-06-13 11:34:16 -0300

    corrigindo o nome do schema gerado.

--- script/schema.dump.sh
+++ script/schema.dump.sh
@@ -4,7 +4,7 @@ if [ -d "script" ]; then
   cd script;
 fi
 
-perl votolegal_create.pl model DB DBIC::Schema SMM::Schema create=static components=TimeStamp,PassphraseColumn 'dbi:Pg:dbname=votolegal_dev;host=localhost' postgres 123mudar quote_names=1 overwrite_modifications=1
+perl votolegal_create.pl model DB DBIC::Schema VotoLegal::Schema create=static components=TimeStamp,PassphraseColumn 'dbi:Pg:dbname=votolegal_dev;host=localhost' postgres 123mudar quote_names=1 overwrite_modifications=1
 
 cd ..;
 
