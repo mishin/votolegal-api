485a0e7436ed2f187ac5377651a6e20e28acc147
Junior Moraes <juniorfvox@gmail.com>
2016-06-14 13:10:46 -0300

    init da controller API::Party.

---
+++ lib/VotoLegal/Controller/API/Party.pm
@@ -0,0 +1,34 @@
+package VotoLegal::Controller::API::Party;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+VotoLegal::Controller::API::Party - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+=encoding utf8
+
+=head1 AUTHOR
+
+Junior Moraes,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
