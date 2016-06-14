cd82a46256e136b31982b27bc29c47b348e52cea
Junior Moraes <juniorfvox@gmail.com>
2016-06-14 15:24:44 -0300

    VotoLegal::Types para as validações.

---
+++ lib/VotoLegal/Types.pm
@@ -0,0 +1,58 @@
+package VotoLegal::Types;
+use common::sense;
+
+use MooseX::Types -declare => [
+  qw(
+    NotTooBigString Int CEP
+    CPF CNPJ PositiveInt
+  )
+];
+
+use MooseX::Types::Common::String qw(NonEmptySimpleStr NonEmptyStr);
+use MooseX::Types::Moose qw(Str Int ArrayRef ScalarRef Num);
+use Moose::Util::TypeConstraints;
+use DateTimeX::Easy qw(parse);
+use MooseX::Types::Structured qw(Dict Tuple Optional);
+use MooseX::Types::DateTime::MoreCoercions qw( DateTime );
+use Business::BR::CEP qw(test_cep);
+use Business::BR::CPF qw(test_cpf);
+use Business::BR::CNPJ qw(test_cnpj);
+
+use DateTime;
+use DateTime::Format::Pg;
+
+subtype PositiveInt,
+    as Int,
+    where { $_ >= 0 },
+  message { "Int is not larger than 0" };
+
+subtype CPF, as NonEmptyStr, where {
+  my $cpf = $_;
+  $cpf =~ s/\D+//g;
+  $cpf !~ /^0+$/ && test_cpf($cpf);
+};
+
+subtype CNPJ, as NonEmptyStr, where {
+  my $cnpj = $_;
+  $cnpj =~ s/\D+//g;
+  $cnpj !~ /^0+$/ && test_cnpj($cnpj);
+};
+
+subtype NotTooBigString, as NonEmptyStr, where {
+  length($_) <= 1024
+}, message { "'$_[0]' is too long (max length 1024)" };
+
+subtype CEP, as Str, where {
+  my $cep = $_;
+  $cep =~ s/\D+//g;
+  $cep =~ s/^(\d+)(\d{3})$/$1-$2/;
+  return test_cep($cep);
+}, message { "$_[0] is not a valid CEP" };
+
+coerce CEP, from Str, via {
+  s/\D+//g;
+
+  #  s/^(\d+)(\d{3})$/$1-$2/;
+  $_
+};
+
