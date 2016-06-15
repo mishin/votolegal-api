23d1a09b15e768d26d2a93e05471aa8e7fc0b704
Junior Moraes <juniorfvox@gmail.com>
2016-06-15 14:45:34 -0300

    GET de Candidate com username.

--- lib/VotoLegal/Controller/API/Candidate.pm
+++ lib/VotoLegal/Controller/API/Candidate.pm
@@ -4,6 +4,8 @@ use namespace::autoclean;
 
 BEGIN { extends 'CatalystX::Eta::Controller::REST' }
 
+use DDP;
+
 =head1 NAME
 
 VotoLegal::Controller::API::Candidate - Catalyst Controller
@@ -16,9 +18,41 @@ Catalyst Controller.
 
 =cut
 
-sub root : Chained('/api/root') : PathPart('candidate') : CaptureArgs(0) { }
+sub root : Chained('/api/root') : PathPart('candidate') : CaptureArgs(1) {
+    my ($self, $c, $username) = @_;
 
-sub base : Chained('/api/logged') : PathPart('candidate') : CaptureArgs(0) { }
+    my $user = $c->model('DB::User')->search({ username => $username })->single;
+
+    if (!$user) {
+        $self->status_bad_request($c, message => "Candidate not found");
+        $c->detach();
+    }
+
+    $c->stash->{collection} = $user->candidates;
+}
+
+sub candidate : Chained('root') : PathPart('') : ActionClass('REST') { }
+
+sub candidate_GET {
+    my ($self, $c) = @_;
+
+    my $candidate = $c->stash->{collection}->search(
+        {},
+        {
+            columns      => [ qw(id name popular_name ficha_limpa raising_goal reelection office_id) ],
+            result_class => 'DBIx::Class::ResultClass::HashRefInflator'
+        }
+    )->single;
+
+    if (!%{$candidate}) {
+        $self->status_bad_request($c, message => 'Candidate not found');
+        $c->detach();
+    }
+
+    return $self->status_ok($c, entity => {
+        candidate => $candidate,
+    });
+}
 
 =encoding utf8
 
