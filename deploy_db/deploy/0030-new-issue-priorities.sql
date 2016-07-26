f7d4b6aa675505bd6c38e3c04752ccc9bbe87ed3
Junior Moraes <juniorfvox@gmail.com>
2016-07-26 14:15:50 -0300

    nova lista de assuntos prioritários.

---
+++ deploy_db/deploy/0030-new-issue-priorities.sql
@@ -0,0 +1,10 @@
+-- Deploy votolegal:0030-new-issue-priorities to pg
+-- requires: 0029-issue-priorities-prod
+
+BEGIN;
+
+TRUNCATE candidate_issue_priority, issue_priority RESTART IDENTITY ;
+
+INSERT INTO issue_priority (name) VALUES ('Acessibilidade'), ('Acesso à internet'), ('Agricultura urbana'), ('Agropecuária'), ('Alimentação saudável'), ('Apoio às pequenas e medias empresas'), ('Compras públicas sustentáveis'), ('Construção sustentável'), ('Cultura'), ('Cultura de paz'), ('Democracia direta'), ('Democracia participativa'), ('Direitos dos animais'), ('Economia criativa'), ('Educação'), ('Eficiência energética'), ('Energias renováveis'), ('Espaços verdes e biodiversidade'), ('Esporte'), ('Esporte educacional'), ('Esporte educacional'), ('Formação profissional'), ('Gestão da água'), ('Gestão de resíduos e reciclagem inclusiva'), ('Gestão integrada e eficiente'), ('Gestão participativa'), ('Governança metropolitana'), ('Habitação'), ('Igualdade entre os gêneros'), ('Igualdade entre raças e etnias'), ('Inclusão digital'), ('Inclusão social'), ('Meio ambiente'), ('Monitoramento e prestação de contas'), ('Mudanças climáticas'), ('Oportunidades de emprego'), ('Planejamento e desenho urbano'), ('Programas para prevenir e superar a condição de pobreza'), ('Qualidade do ar'), ('Qualidade dos serviços públicos'), ('Respeito à diversidade sexual'), ('Saúde'), ('Segurança'), ('Transparência e dados abertos'), ('Transporte individual motorizado'), ('Transporte público'), ('Transportes ativos (pedestres e ciclistas)'), ('Veículos menos poluentes') ;
+
+COMMIT;
