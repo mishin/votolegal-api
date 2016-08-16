7a567d8c08f9274feaf9ee942a87305140bd729f
Junior Moraes <juniorfvox@gmail.com>
2016-08-16 17:19:14 -0300

    fix: travando caracteres especiais no username.

--- lib/VotoLegal/Schema/ResultSet/Candidate.pm
+++ lib/VotoLegal/Schema/ResultSet/Candidate.pm
@@ -46,6 +46,8 @@ sub verifiers_specs {
 
                         my $username = $r->get_value('username');
 
+                        $username =~ m{^[a-zA-Z0-9\._-]+$} or die \['username', 'invalid characters'];
+
                         if ($username !~ m{[a-zA-Z]}) {
                             die \['username', "must have letters"];
                         }
