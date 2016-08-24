933610797406a513b57af5dd3d5e237bc3c811ac
Junior Moraes <juniorfvox@gmail.com>
2016-08-24 17:05:48 -0300

    script para enviar estatísticas das doações por slack.

---
+++ script/donation_stats_slack.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+use common::sense;
+use FindBin qw($RealBin $Script);
+use lib "$RealBin/../lib";
+
+use Config::General;
+use WebService::Slack::IncomingWebHook;
+use VotoLegal::Schema;
+
+# Config.
+my $config = new Config::General("$RealBin/../votolegal.conf");
+$config = { $config->getall };
+
+my $schema = VotoLegal::Schema->connect($config->{model}->{DB}->{connect_info});
+
+my $donation_rs = $schema->resultset('Donation')->search(
+    { "me.status" => "captured" },
+    {   
+        join     => "candidate",
+        select   => [ "candidate.name", { sum => "amount", '-as' => "total_amount" } ],
+        as       => [ qw(candidate_name total_amount) ],
+        group_by => [ "candidate_id", "candidate.name" ],
+        order_by => { '-desc' => "total_amount" },
+    },  
+);
+
+my $post = "Total de doações recebidas por candidato: \n\n";
+
+while (my $donation = $donation_rs->next()) {
+    $post .= $donation->get_column('candidate_name');
+    $post .= ": R\$ ";
+    $post .= sprintf("%.2f", ($donation->get_column("total_amount") / 100));
+    $post .= "\n";
+}
+
+my $slack = WebService::Slack::IncomingWebHook->new(%{$config->{slack}});
+
+$slack->post(text => $post);
+
