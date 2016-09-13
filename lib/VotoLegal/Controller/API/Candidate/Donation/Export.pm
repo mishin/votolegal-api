7c83446672740bd38f6529e6a8e5764d142e95c0
Junior Moraes <juniorfvox@gmail.com>
2016-09-13 17:44:58 -0300

    implementando exportações das doações pro sistema do tse.

---
+++ lib/VotoLegal/Controller/API/Candidate/Donation/Export.pm
@@ -0,0 +1,71 @@
+package VotoLegal::Controller::API::Candidate::Donation::Export;
+use common::sense;
+use Moose;
+use namespace::autoclean;
+
+use File::Temp ':seekable';
+
+BEGIN { extends "Catalyst::Controller" }
+
+with "CatalystX::Eta::Controller::TypesValidation";
+
+sub root : Chained('/api/candidate/donation/base') : PathPart('') : CaptureArgs(0) {
+    my ($self, $c) = @_;
+
+    $c->stash->{collection} = $c->stash->{candidate}->donations;
+}
+
+sub base : Chained('root') : PathPart('export') : CaptureArgs(0) { }
+
+sub export : Chained('base') : PathPart('') : Args(0) {
+    my ($self, $c) = @_;
+
+    $self->validate_request_params(
+        $c,
+        date => {
+            type       => "Str",
+            required   => 1,
+        },
+        receipt_id => {
+            type     => "Int",
+            required => 1,
+        },
+    );
+
+    my $date       = $c->req->params->{date};
+    my $receipt_id = $c->req->params->{receipt_id};
+
+    $c->stash->{collection} = $c->stash->{collection}->search(\[
+        'CAST(captured_at AS DATE) = ?',
+        $date,
+    ]);
+
+    $c->stash->{collection} = $c->stash->{collection}->search({ status => "captured" });
+
+    my $filehandle = $c->stash->{collection}->export($receipt_id);
+    $filehandle->seek(0, SEEK_SET);
+
+    $c->response->content_type("text/plain");
+    $c->response->headers->header("content-disposition" => "attachment;filename=doacoes.txt");
+
+    $c->res->body($filehandle);
+
+    $c->detach();
+}
+
+=encoding utf8
+
+=head1 AUTHOR
+
+Junior Moraes,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
--- lib/VotoLegal/Schema/ResultSet/Donation.pm
+++ lib/VotoLegal/Schema/ResultSet/Donation.pm
@@ -8,7 +8,7 @@ extends 'DBIx::Class::ResultSet';
 with 'VotoLegal::Role::Verification';
 
 use Text::CSV;
-use File::Temp;
+use File::Temp ':seekable';
 use Time::HiRes;
 use Digest::MD5 qw(md5_hex);
 use Data::Verifier;
@@ -299,6 +299,84 @@ sub action_specs {
             return ;
         },
     };
+}
+
+sub export {
+    my ($self, $receipt_id) = @_;
+
+    my $fh = File::Temp->new(UNLINK => 1);
+    $fh->autoflush(1);
+
+    my $count       = 0;
+    my $writeHeader = 1;
+    while (my $donation = $self->next()) {
+        # Tratando alguns campos do candidato.
+        my $cnpj                = $donation->candidate->cnpj;
+        $cnpj                   =~ s/\D+//g;
+        $cnpj                   = left_padding_zeros($cnpj, 14);
+        my $data_movimentacao   = DateTime->now(time_zone => "America/Sao_Paulo")->strftime("%d%m%Y%H%M");
+        my $bank_code           = left_padding_zeros($donation->candidate->bank_code, 3);
+        my $bank_agency         = left_padding_zeros($donation->candidate->bank_agency, 8);
+        my $bank_agency_dv      = left_padding_zeros($donation->candidate->bank_agency_dv, 2);
+        my $bank_account_number = left_padding_zeros($donation->candidate->bank_account_number, 18);
+        my $bank_account_dv     = left_padding_zeros($donation->candidate->bank_account_dv, 2);
+
+        # Escrevendo o header.
+        if ($writeHeader) {
+            print $fh "1";                     # Registro.
+            print $fh $cnpj;                   # CNPJ.
+            print $fh $data_movimentacao;      # Data da movimentação.
+            print $fh $bank_code;              # Código do banco.
+            print $fh $bank_agency;            # Numero da agência.
+            print $fh $bank_agency_dv;         # Dígito verificador da agência.
+            print $fh $bank_account_number;    # Número da conta.
+            print $fh $bank_account_dv;        # Digito verificador da conta.
+            print $fh "400";                   # Versao do layout.
+            print $fh "DOACINTE";              # Nome do layout.
+            print $fh " " x 93;                # Preencher com espaços em branco.
+            print $fh "\r\n";
+
+            $writeHeader = 0;
+        }
+
+        # Tratando os dados da doação.
+        $receipt_id                    = left_padding_zeros($receipt_id, 21);
+        my $payment_gateway_code       = $donation->payment_gateway_code;
+        $payment_gateway_code          =~ s/-//g;
+        my ($doc_number, $auth_number) = unpack "(A16)*", $payment_gateway_code;
+        $doc_number                    = left_padding_zeros($doc_number, 20);
+        $auth_number                   = left_padding_zeros($auth_number, 20);
+        my $cpf                        = left_padding_zeros($donation->cpf, 11);
+        my $name                       = left_padding_whitespaces($donation->name, 60);
+        my $captured_at                = $donation->captured_at->strftime('%m%d%Y');
+        my $amount                     = sprintf("%.2f", $donation->amount / 100);
+        $amount                        =~ s/\.//;
+        $amount                        = left_padding_zeros($amount, 18);
+
+        print $fh "2";                  # Registro.
+        print $fh $receipt_id;          # Id do recibo.
+        print $fh $doc_number;          # Numero do documento.
+        print $fh $auth_number;         # Numero do documento.
+        print $fh "01";                 # Tipo da doação. TODO Duvida.
+        print $fh "02";                 # Espécie do recurso: cartão de crédito.
+        print $fh $cpf;                 # CPF do doador.
+        print $fh "F";                  # Pessoa física.
+        print $fh $captured_at;         # Data da doação.
+        print $fh $amount;              # Valor da doação.
+
+        # Fim da doação.
+        print $fh "\r\n";
+
+        $count++;
+        $receipt_id++;
+    }
+
+    # Trailer.
+    print $fh "3";                           # Registro.
+    print $fh left_padding_zeros($count, 9); # Total de doações presentes neste arquivo.
+    print $fh " " x 154;                     # Espaços em branco.
+
+    return $fh;
 }
 
 1;
