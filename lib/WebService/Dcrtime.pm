d6a20e33df4804f91bca97e0a56ecc5fe039311d
Junior Moraes <carlos@eokoe.com>
2018-05-20 20:34:32 -0300

    Remove fake data, use testnet server
    VL20-112 #time 15m

--- lib/WebService/Dcrtime.pm
+++ lib/WebService/Dcrtime.pm
@@ -17,34 +17,42 @@ BEGIN { defined($ENV{VOTOLEGAL_DCRTIME_API}) or die "Missing env 'VOTOLEGAL_DCRT
 sub timestamp {
     my ($self, %opts) = @_;
 
-    #if (is_test()) {
-    if (0) {
-        return {
-            id      => "votolegal",
-            results => [ 1 ],
-            digests => [ "c6b52210addac4bde43c8ce66de95888f185925f3281af960c172c7c3c19b875" ],
-            servertimestamp => 1526839200,
-        };
-    }
-    else {
-        my $res;
-        eval {
-            retry {
-                $res = $self->furl->put( $ENV{VOTOLEGAL_DCRTIME_API} . '/v1/timestamp', [], [ encode_json(%opts) ] );
-                die $res->decoded_content unless $res->is_success;
-            }
-            retry_if { shift() < 3 } catch { die $_; };
-        };
+    my $res;
+    eval {
+        retry {
+            $res = $self->ua->post( $ENV{VOTOLEGAL_DCRTIME_API} . '/v1/timestamp/', [], encode_json(\%opts) );
+            die $res->decoded_content unless $res->is_success;
+        }
+        retry_if { shift() < 3 } catch { die $_; };
+    };
 
-        die "Error: $@" if $@;
-        die "Cannot call Dcrtime" unless ref $res;
-        die "Request failed: " . $res->as_string unless $res->is_success;
+    die "Error: $@" if $@;
+    die "Cannot call Dcrtime" unless ref $res;
+    die "Request failed: " . $res->as_string unless $res->is_success;
 
-        return decode_json( $res->decoded_content );
-    }
+    return decode_json( $res->decoded_content );
 }
 
-sub _build_furl { Furl->new( timeout => 30 ) }
+sub verify {
+    my ($self, %opts) = @_;
+
+    my $res;
+    eval {
+        retry {
+            $res = $self->ua->post( $ENV{VOTOLEGAL_DCRTIME_API} . '/v1/verify/', [], encode_json(\%opts) );
+            die $res->decoded_content unless $res->is_success;
+        }
+        retry_if { shift() < 3 } catch { die $_; };
+    };
+
+    die "Error: $@" if $@;
+    die "Cannot call Dcrtime" unless ref $res;
+    die "Request failed: " . $res->as_string unless $res->is_success;
+
+    return decode_json( $res->decoded_content );
+}
+
+sub _build_ua { Furl->new( timeout => 30 ) }
 
 __PACKAGE__->meta->make_immutable;
 
