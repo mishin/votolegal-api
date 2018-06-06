2bb139aa7c58d24b2890635e23419a30516da8a6
Lucas Ansei <lucas.ansei@eokoe.com>
2018-06-06 16:21:45 -0300

    Updating donation export

--- lib/VotoLegal/Controller/API/Candidate/Donation/Download.pm
+++ lib/VotoLegal/Controller/API/Candidate/Donation/Download.pm
@@ -34,14 +34,17 @@ sub csv : Chained('base') : PathPart('csv') : Args(0) {
 				'me.is_pre_campaign',
 				'me.decred_merkle_root',
 				'me.decred_capture_txid',
-				'candidate.campaign_donation_type',
-				'candidate.cpf',
-				'candidate.name',
-				'candidate.cnpj',
-				{ party_name  => 'party.name' },
-				{ donor_name  => 'votolegal_donation_immutable.donor_name' },
-				{ donor_cpf   => 'votolegal_donation_immutable.donor_cpf' },
-				{ donor_email => 'votolegal_donation_immutable.donor_email' },
+				{ donor_name                         => 'votolegal_donation_immutable.donor_name' },
+				{ donor_cpf                          => 'votolegal_donation_immutable.donor_cpf' },
+				{ donor_email                        => 'votolegal_donation_immutable.donor_email' },
+				{ donor_birthdate                    => 'votolegal_donation_immutable.donor_birthdate' },
+                { donor_billing_address_state        => 'votolegal_donation_immutable.billing_address_state' },
+                { donor_billing_address_city         => 'votolegal_donation_immutable.billing_address_city' },
+				{ donor_billing_address_zipcode      => 'votolegal_donation_immutable.billing_address_zipcode' },
+				{ donor_billing_address_district     => 'votolegal_donation_immutable.billing_address_district' },
+				{ donor_billing_address_street       => 'votolegal_donation_immutable.billing_address_district' },
+				{ donor_billing_address_house_number => 'votolegal_donation_immutable.billing_address_house_number' },
+				{ donor_billing_address_complement   => 'votolegal_donation_immutable.billing_address_complement' },
 				{
 					amount_human => \"replace((votolegal_donation_immutable.amount/100)::numeric(7, 2)::text, '.', ',')"
 				},
@@ -77,50 +80,66 @@ sub csv : Chained('base') : PathPart('csv') : Args(0) {
         $fh,
         [
             qw(
-              NAME
-              CPF
-              EMAIL
-              PHONE
-              STATE
-              CITY
-              ADDRESS
-              AMOUNT
-              BIRTHDATE
-              DATE
+                DOACAO_DE_PRE_CAMPANHA
+                METODO
+                NOME
+                CPF
+                EMAIL
+                TELEFONE
+                ESTADO
+                CIDADE
+                CEP
+                BAIRRO
+                RUA
+                NUMERO
+                COMPLEMENTO
+                VALOR
+                NASCIMENTO
+                DECRED_MERKLE_ROOT
+                DECRED_CAPTURE_TXID
+                DATA_DE_CRIAÇÃO
+                DATA_DE_CAPTURA
+                DATA_DE_ESTORNO
               )
         ]
     );
 
     while ( my $votolegal_donation = $votolegal_donation_rs->next() ) {
-        use DDP; p $votolegal_donation;
-        my $address_street       = $votolegal_donation->address_street       || "";
-        my $address_complement   = $votolegal_donation->address_complement   || "";
-        my $address_house_number = $votolegal_donation->address_house_number || "";
 
         $csv->print(
             $fh,
             [
-                $votolegal_donation->name,
-                $votolegal_donation->cpf,
-                $votolegal_donation->email,
-                $votolegal_donation->phone,
-                $votolegal_donation->address_state,
-                $votolegal_donation->address_city,
-                sprintf( "%s %s, %s", $address_street, $address_complement, $address_house_number ),
-                sprintf( "%.2f",      $result->amount / 100 ),
-                $votolegal_donation->birthdate->strftime("%d/%m/%Y"),
-                $votolegal_donation->captured_at->strftime("%d/%m/%Y %H:%M:%S"),
+                $votolegal_donation->{is_pre_campaign},
+                $votolegal_donation->{payment_method_human},
+                $votolegal_donation->{donor_name},
+                $votolegal_donation->{donor_cpf},
+                $votolegal_donation->{donor_email},
+                $votolegal_donation->{donor_phone},
+                $votolegal_donation->{donor_billing_address_state},
+                $votolegal_donation->{donor_billing_address_city},
+                $votolegal_donation->{donor_billing_address_zipcode},
+                $votolegal_donation->{donor_billing_address_district},
+                $votolegal_donation->{donor_billing_address_street},
+                $votolegal_donation->{donor_billing_address_house_number},
+                $votolegal_donation->{donor_billing_address_complement},
+                $votolegal_donation->{amount_human},
+                $votolegal_donation->{donor_birthdate},
+                $votolegal_donation->{decred_merkle_root},
+                $votolegal_donation->{decred_capture_txid},
+                $votolegal_donation->{created_at_human},
+                $votolegal_donation->{captured_at_human},
+				$votolegal_donation->{refunded_at_human},
             ]
         );
     }
 
-    # binmode( $fh, ":raw" );
-    # $fh->seek( 0, SEEK_SET );
+    binmode( $fh, ":raw" );
+    $fh->seek( 0, SEEK_SET );
 
-    # $c->response->headers->header( "content-disposition" => "attachment;filename=" . basename( $fh->filename ) );
-    # $c->response->content_type("text/csv");
-    # $c->res->body($fh);
-    # $c->detach();
+    $c->response->headers->header( "content-disposition" => "attachment;filename=" . basename( $fh->filename ) );
+    $c->response->content_type("text/csv");
+    $c->res->body($fh);
+    $c->detach();
 }
 
 =encoding utf8
