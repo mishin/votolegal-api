86dceacb845cbf2c63bb5127b450dc63574e1a42
Lucas Ansei <lucas.ansei@eokoe.com>
2018-06-11 15:21:07 -0300

    Fix: if statement

--- lib/VotoLegal/Controller/API/Login.pm
+++ lib/VotoLegal/Controller/API/Login.pm
@@ -63,8 +63,7 @@ sub login_POST {
                         $value = '297.00';
                     }
                     elsif (( $candidate->party_id == 34 && $candidate->political_movement_id != 1 )
-                        || ( $candidate->political_movement_id && $candidate->political_movement_id =~ /^(2|3|4|5|8)$/ )
-                        || $candidate->party_id == 4 )
+                        || ( $candidate->political_movement_id && $candidate->political_movement_id =~ /^(2|3|4|5|8)$/ ) )
                     {
                         $value = '396.00';
                     }
@@ -72,6 +71,9 @@ sub login_POST {
                 elsif ( $candidate->party_id == 26 ) {
                     $value = '297.00';
                 }
+				elsif ( $candidate->party_id == 4 ) {
+					$value = '396.00';
+				}
                 else {
                     $value = '495.00';
                 }
