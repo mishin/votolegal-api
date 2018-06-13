d13895e5d6c63ee8c03075168f107faccf0a6f49
Lucas Ansei <lucas.ansei@eokoe.com>
2018-06-13 10:37:22 -0300

    Resetting sqitch

--- deploy_db/deploy/0132-procob-configs.sql
+++
@@ -1,9 +0,0 @@
--- Deploy votolegal:0132-procob-configs to pg
--- requires: 0131-procob
-
-BEGIN;
-
-INSERT INTO config (name, value) VALUES ('PROCOB_ENABLED', '1'), ('PROCOB_AUTH', 'TOKEN'), ('PROCOB_API_URL', 'https://api.procob.com/');
-
-
-COMMIT;
