ef4f5850b2bf2432ef3ca7c3ba71d649f4a757d3
Lucas Ansei <lucas.ansei@eokoe.com>
2018-06-25 13:54:19 -0300

    Adding candidate avatar script

---
+++ script/get-candidate-avatar.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+use common::sense;
+use FindBin qw($RealBin $Script);
+use lib "$RealBin/../lib";
+use Moose;
+use namespace::autoclean;
+
+use File::MimeInfo;
+use LWP::Simple;
+use Imager;
+
+use VotoLegal::SchemaConnected;
+use VotoLegal::Uploader;
+
+my $schema   = get_schema;
+my $uploader = VotoLegal::Uploader->new();
+
+my $candidate_rs = $schema->resultset("Candidate")->search( { avatar => \'IS NULL' } );
+
+for my $candidate ( $candidate_rs->next() ) {
+    my $picture = $candidate->picture;
+
+    my $tmp_pic = $candidate->id . '_resized';
+
+    getstore($picture, $tmp_pic);
+
+    resize_image($tmp_pic);
+
+    my $avatar = upload_picture($tmp_pic);
+
+    $candidate->update( { avatar => $avatar } );
+}
+
+sub resize_image {
+    my ($image) = @_;
+
+	my $conv = `convert $image -resize 180x180 $image`;
+
+    return $image;
+}
+
+sub upload_picture {
+    my ($uploader, $picture) = @_;
+
+    my $mimetype = mimetype( $picture );
+	die \[ 'picture', 'invalid image' ] unless $mimetype =~ m{^image\/};
+
+	my $avatar_url = $uploader->upload(
+		{
+			path => $picture,
+			file => $picture,
+			type => $mimetype,
+		}
+	);
+
+    return $avatar_url->as_string;
+}
