6863f33170846a8c9f5f45aa9bc063f5f7f0d428
Lucas Ansei <lucas.ansei@eokoe.com>
2018-07-03 16:38:00 -0300

    Removing address_state_name from query

--- lib/VotoLegal/Controller/PublicAPI/CandidateSummary.pm
+++ lib/VotoLegal/Controller/PublicAPI/CandidateSummary.pm
@@ -16,7 +16,7 @@ sub root : Chained('/publicapi/root') : PathPart('candidate-summary') : CaptureA
         },
         {
             '+columns' => {
-                address_state_name            => \'(select name from state x where x.code = COALESCE( me.running_for_address_state, me.address_state ) limit 1)',
+                # address_state_name            => \'(select name from state x where x.code = COALESCE( me.running_for_address_state, me.address_state ) limit 1)',
                 has_mandatoaberto_integration => \
                   'EXISTS (select 1 from candidate_mandato_aberto_integration x where x.candidate_id = me.id)'
             },
@@ -70,7 +70,7 @@ sub candidate_GET {
           qw(
           id name popular_name status reelection party_id office_id status username picture color publish
           video_url facebook_url twitter_url website_url summary biography instagram_url cnpj cpf
-          raising_goal public_email spending_spreadsheet address_city address_state address_state_name
+          raising_goal public_email spending_spreadsheet address_city address_state
           political_movement_id google_analytics collect_donor_phone collect_donor_address
           min_donation_value
           )
