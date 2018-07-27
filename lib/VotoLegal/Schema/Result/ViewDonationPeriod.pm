761625edd52d2beb7a312760fa96838dd235f45c
Lucas Ansei <lucas.ansei@eokoe.com>
2018-07-27 15:54:25 -0300

    Adding donation count by boleto and CC

--- lib/VotoLegal/Schema/Result/ViewDonationPeriod.pm
+++ lib/VotoLegal/Schema/Result/ViewDonationPeriod.pm
@@ -12,7 +12,8 @@ __PACKAGE__->add_columns(qw(
     candidate_id candidate_name address_state
     days_fundraising amount_raised donation_count
     raising_goal median_per_day party avg_donation_amount
-    goal_raised_percentage amount_boleto amount_credit_card)
+    goal_raised_percentage amount_boleto amount_credit_card
+    count_credit_card count_boleto)
 );
 
 # do not attempt to deploy() this view
@@ -36,7 +37,9 @@ SELECT
     ) as avg_donation_amount,
     ( ( ( s.amount_donation_by_votolegal / 100 ) / c.raising_goal ) * 100 )::numeric(11, 3) as goal_raised_percentage,
     ( select sum(amount) from votolegal_donation_immutable i, votolegal_donation d where d.is_boleto = true and d.captured_at is not null and d.id = i.votolegal_donation_id and d.candidate_id = c.id  and d.refunded_at is null) / 100 as amount_boleto,
-    ( select sum(amount) from votolegal_donation_immutable i, votolegal_donation d where d.is_boleto = false and d.captured_at is not null and d.id = i.votolegal_donation_id and d.candidate_id = c.id and d.refunded_at is null ) / 100 as amount_credit_card
+    ( select count(1) from votolegal_donation where is_boleto = false and captured_at is not null and refunded_at is null and candidate_id = c.id) as count_credit_card,
+    ( select sum(amount) from votolegal_donation_immutable i, votolegal_donation d where d.is_boleto = false and d.captured_at is not null and d.id = i.votolegal_donation_id and d.candidate_id = c.id and d.refunded_at is null ) / 100 as amount_credit_card,
+	( select count(1) from votolegal_donation where is_boleto = true and captured_at is not null and refunded_at is null and candidate_id = c.id) as count_boleto
 FROM candidate AS c, candidate_donation_summary AS s, party AS p
 WHERE c.party_id = p.id AND s.candidate_id = c.id AND c.is_published = true
     AND c.name NOT ILIKE '%Edgard%' AND c.name NOT ILIKE '%Lucas Ansei%'
